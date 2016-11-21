c779b77c48948f6dd8c288dfe6666822d5a4fb2a
joseph park <jpark041@gmail.com>
2016-11-20 23:45:20 -0800

    appstore submission version 1.0

---
+++ Calculyeahtor/Calculyeahtor/ViewController.swift
@@ -0,0 +1,175 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Joseph Park on 11/15/16.
+//  Copyright © 2016 Joseph Park. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+public class ViewController: UIViewController {
+    var buttonSound: AVAudioPlayer!
+    var runningNumber = ""
+    var currentOperation = Operation.Empty
+    var leftValueString = ""
+    var rightValueString = ""
+    var result = ""
+    var userCurrentlyTypingNum = false
+    
+    @IBOutlet var evenNumbersPressed: [UIButton]!
+    @IBOutlet var oddNumbersPressed: [UIButton]!
+    @IBOutlet weak var zeroIsPressed: UIButton!
+    @IBOutlet weak var outputLabel: UILabel!
+
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        runningNumber = ""
+        outputLabel.text = "0"
+        currentOperation = Operation.Empty
+    }
+    
+
+    //calculte logic
+    enum Operation: String {
+        case Multiply = "*"
+        case Divide = "/"
+        case Add = "+"
+        case Subtract = "-"
+        case Empty = "Empty"
+        
+    }
+    
+    @IBAction func onDividePressed (sender: AnyObject) {
+        processOperation(operation:.Divide)
+    }
+    @IBAction func onMultiplyPressed (sender: AnyObject) {
+        processOperation(operation:.Multiply)
+    }
+    @IBAction func onAddPressed (sender: AnyObject) {
+        processOperation(operation:.Add)
+    }
+    @IBAction func onSubtractPressed (sender: AnyObject) {
+        processOperation(operation:.Subtract)
+    }
+    @IBAction func onEqualsPressed(sender: AnyObject) {
+        processOperation(operation: currentOperation)
+    }
+    @IBAction func onClearPressed(Sender: UIButton) {
+        let yeahClearPath = Bundle.main.path(forResource: "yeahClear", ofType: "wav")
+        let yeahClearURL = URL(fileURLWithPath: yeahClearPath!)
+        if evenNumbersPressed != nil {
+            do {
+                try buttonSound = AVAudioPlayer(contentsOf: yeahClearURL)
+                buttonSound.prepareToPlay()
+            } catch let err as NSError {
+                print(err.debugDescription)
+            }
+        }
+        playSound()
+        runningNumber = ""
+        outputLabel.text = "0"
+        currentOperation = Operation.Empty
+    }
+    
+    @IBAction func numberPressed(sender: UIButton) {
+        if currentOperation == Operation.Empty {
+            if sender.tag == 0 && runningNumber == "" {
+                return
+            }
+        }
+        runningNumber += "\(sender.tag)"
+        outputLabel.text = runningNumber
+
+        }
+
+    func processOperation(operation: Operation) {
+        if leftValueString != "" && currentOperation != Operation.Empty {
+            
+            //when user selects an operator, then selects another operator without first entering a number
+            if runningNumber != "" {
+                rightValueString = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValueString)! * Double(rightValueString)!)"
+                }
+                else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValueString)! / Double(rightValueString)!)"
+                }
+                else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValueString)! + Double(rightValueString)!)"
+                }
+                else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValueString)! - Double(rightValueString)!)"
+                }
+                leftValueString = result
+                outputLabel.text = result
+            }
+            currentOperation = operation
+        }
+        else {
+            //First time an operator has been pressed
+            leftValueString = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+    }
+    
+    
+    //music play logic
+    @IBAction func evenNumberPressed(sender: UIButton) {
+        let yeahUpPath = Bundle.main.path(forResource: "yeahUp", ofType: "wav")
+        let yeahUpSoundURL = URL(fileURLWithPath: yeahUpPath!)
+        if evenNumbersPressed != nil {
+            do {
+                try buttonSound = AVAudioPlayer(contentsOf: yeahUpSoundURL)
+                buttonSound.prepareToPlay()
+            } catch let err as NSError {
+                print(err.debugDescription)
+            }
+        }
+        playSound()
+    }
+    
+    @IBAction func oddNumberPressed(sender: UIButton) {
+        let yeahDownPath = Bundle.main.path(forResource: "yeahDown", ofType: "wav")
+        let yeahDownSoundURL = URL(fileURLWithPath: yeahDownPath!)
+
+        if oddNumbersPressed != nil {
+            do {
+                try buttonSound = AVAudioPlayer(contentsOf: yeahDownSoundURL)
+                buttonSound.prepareToPlay()
+            } catch let err as NSError {
+                print(err.debugDescription)
+            }
+        }
+        playSound()
+    }
+    
+    @IBAction func zeroPressed(sender: UIButton) {
+        let yeahPath = Bundle.main.path(forResource: "yeah", ofType: "wav")
+        let yeahSoundURL = URL(fileURLWithPath: yeahPath!)
+
+        if zeroIsPressed != nil {
+            do {
+                try buttonSound = AVAudioPlayer(contentsOf: yeahSoundURL)
+                buttonSound.prepareToPlay()
+            } catch let err as NSError {
+                print(err.debugDescription)
+            }
+        }
+        playSound()
+    }
+    
+    func playSound() {
+        if buttonSound.isPlaying {
+            buttonSound.stop()
+        }
+        buttonSound.play()
+    }
+    
+}
