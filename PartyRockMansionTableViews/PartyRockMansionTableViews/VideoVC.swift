343a992453bfce419493ceb947d0164f6120146f
joseph park <jpark041@gmail.com>
2016-11-22 16:59:02 -0800

    Assets imported, Cells made in Tableview, Webview UI made

---
+++ PartyRockMansionTableViews/PartyRockMansionTableViews/VideoVC.swift
@@ -0,0 +1,35 @@
+//
+//  VideoVC.swift
+//  PartyRockMansionTableViews
+//
+//  Created by Joseph Park on 11/22/16.
+//  Copyright © 2016 Joseph Park. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
