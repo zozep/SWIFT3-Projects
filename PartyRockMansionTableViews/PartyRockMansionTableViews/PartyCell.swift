ed622e575e0d20984701749959ea69f0bf4e6097
joseph park <jpark041@gmail.com>
2016-11-25 22:31:40 -0800

    preview images set correctly, doesn’t throw errors

--- PartyRockMansionTableViews/PartyRockMansionTableViews/PartyCell.swift
+++ PartyRockMansionTableViews/PartyRockMansionTableViews/PartyCell.swift
@@ -17,24 +17,19 @@ class PartyCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     
     func updateUI(partyRock: PartyRock) {
         videoTitle.text = partyRock.videoTitle
         
         let url = URL(string: partyRock.imageURL)!
+        
         DispatchQueue.global().async {
             do {
                 let data = try Data(contentsOf: url)
                 DispatchQueue.global().sync {
                     self.videoPreviewImage.image = UIImage(data: data)
                 }
-            } catch { 
+            } catch {
                //handle error
             }
         }
