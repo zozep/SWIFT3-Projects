c9b78b92be34a4739b3e94759b462839cbdab353
joseph park <jpark041@gmail.com>
2016-11-25 22:16:59 -0800

    set correct UIImageView, and set security settings to allow HTTPS transport

--- PartyRockMansionTableViews/PartyRockMansionTableViews/MainVC.swift
+++ PartyRockMansionTableViews/PartyRockMansionTableViews/MainVC.swift
@@ -21,8 +21,18 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         //hardcoded video url
         let p1 = PartyRock(imageURL: "https://i.ytimg.com/vi/ev4bY1SkZLg/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68&sigh=2eQIiU3s4BGW4Q7oDtui_k5Ketc", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/ev4bY1SkZLg\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Let's Get Ridiculous")
         
-        partyRocks.append(p1)
         
+        let p2 = PartyRock(imageURL: "https://i.ytimg.com/vi/ev4bY1SkZLg/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68&sigh=2eQIiU3s4BGW4Q7oDtui_k5Ketc", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/ev4bY1SkZLg\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "New Thang")
+        
+         let p3 = PartyRock(imageURL: "https://i.ytimg.com/vi/ev4bY1SkZLg/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68&sigh=2eQIiU3s4BGW4Q7oDtui_k5Ketc", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/ev4bY1SkZLg\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Shots")
+        
+        let p4 = PartyRock(imageURL: "https://i.ytimg.com/vi/ev4bY1SkZLg/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68&sigh=2eQIiU3s4BGW4Q7oDtui_k5Ketc", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/ev4bY1SkZLg\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Light's Out")
+        
+        partyRocks.append(p1)
+        partyRocks.append(p2)
+        partyRocks.append(p3)
+        partyRocks.append(p4)
+
         tableView.delegate = self
         tableView.dataSource = self
     }
--- PartyRockMansionTableViews/PartyRockMansionTableViews/PartyCell.swift
+++ PartyRockMansionTableViews/PartyRockMansionTableViews/PartyCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class PartyCell: UITableViewCell {
 
-    @IBOutlet weak var videoPreviewImage: UIView!
+    @IBOutlet weak var videoPreviewImage: UIImageView!
     @IBOutlet weak var videoTitle: UILabel!
     
     override func awakeFromNib() {
@@ -26,7 +26,18 @@ class PartyCell: UITableViewCell {
     
     func updateUI(partyRock: PartyRock) {
         videoTitle.text = partyRock.videoTitle
-        //TODO: set image from URL
+        
+        let url = URL(string: partyRock.imageURL)!
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.videoPreviewImage.image = UIImage(data: data)
+                }
+            } catch { 
+               //handle error
+            }
+        }
         
     }
 
