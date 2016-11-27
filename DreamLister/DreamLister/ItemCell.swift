503365df3d9fd8f5b679217ac6698a95e88fee64
joseph park <jpark041@gmail.com>
2016-11-26 19:17:17 -0800

    customcell funcions for tableview

---
+++ DreamLister/DreamLister/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Joseph Park on 11/26/16.
+//  Copyright © 2016 Joseph Park. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+}
--- DreamLister/DreamLister/MainVC.swift
+++ DreamLister/DreamLister/MainVC.swift
@@ -8,18 +8,26 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var segment: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
 
 }
-
