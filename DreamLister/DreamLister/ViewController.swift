76a0235da252ded905d5d5d73913f978e250c048
joseph park <jpark041@gmail.com>
2016-11-26 17:09:27 -0800

    initial commit - exp with core data

---
+++ DreamLister/DreamLister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DreamLister
+//
+//  Created by Joseph Park on 11/26/16.
+//  Copyright © 2016 Joseph Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
