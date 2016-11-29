01aaefb0a23be6b04c87e5cf95fdd4d53903b1d9
joseph park <jpark041@gmail.com>
2016-11-29 14:36:01 -0800

    UIPickerView boilerplate code for ItemDetailsVC

--- DreamLister/DreamLister/ItemDetailsVC.swift
+++ DreamLister/DreamLister/ItemDetailsVC.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ItemDetailsVC: UIViewController {
+class ItemDetailsVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var storePicker: UIPickerview!
+    @IBOutlet weak var titleField: CustomTextfield!
+    @IBOutlet weak var priceField: CustomTextField!
+    @IBOutlet weak var detailsField: CustomTextField!
+    
+    var stores = [Store]()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let topItem = self.navigationController?.navigationBar.topItem {
             topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
         }
+        
+        storePicker.delegate = self
+        storePicker.dataSource = self
+        
     }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let store = stores[row]
+        return store.name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stores.count
+    }
+    
+    //number of columns
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //update when selected
+    }
+    
+    
 }
