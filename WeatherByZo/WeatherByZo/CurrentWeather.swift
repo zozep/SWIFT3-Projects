7f5d3c37c4df08efaf7ca0a59699e251356d3d84
joseph park <jpark041@gmail.com>
2016-11-30 00:41:29 -0800

    CurrentWeather class for safer data access

---
+++ WeatherByZo/WeatherByZo/CurrentWeather.swift
@@ -0,0 +1,42 @@
+//
+//  CurrentWeather.swift
+//  WeatherByZo
+//
+//  Created by Joseph Park on 11/29/16.
+//  Copyright © 2016 Joseph Park. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class CurrentWeather {
+    var _cityName: String!
+    var _date: String!
+    var _weatherType: String!
+    var _currentTemp: Double!
+    
+    var cityName: String {
+        if _cityName == nil {
+            _cityName = ""
+        }
+        return _cityName
+    }
+    var date: String {
+        if _date == nil {
+            _date = ""
+        }
+        return _date
+    }
+    var weatherType: String {
+        if _weatherType == nil {
+            _weatherType = ""
+        }
+        return _weatherType
+    }
+    var currentTemp: Double {
+        if _currentTemp == nil {
+            _currentTemp = 0.0
+        }
+        return _currentTemp
+    }
+}
