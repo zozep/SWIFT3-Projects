db497ee04e55996e55676e71f404b4d0fc41c776
joseph park <jpark041@gmail.com>
2016-11-29 23:25:32 -0800

    boilerplate code for tableview datasource & delegate

--- WeatherByZo/WeatherByZo/WeatherVC.swift
+++ WeatherByZo/WeatherByZo/WeatherVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var currentTempLabel: UILabel!
@@ -20,8 +20,22 @@ class WeatherVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
+    
 }
