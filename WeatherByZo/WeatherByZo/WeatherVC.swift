33f3ac4ae8b9dcb7f92d9688931db3c8da953855
joseph park <jpark041@gmail.com>
2016-11-29 22:25:35 -0800

    weather basic UI set with proper stackViews

---
+++ WeatherByZo/WeatherByZo/WeatherVC.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherVC.swift
+//  WeatherByZo
+//
+//  Created by Joseph Park on 11/29/16.
+//  Copyright © 2016 Joseph Park. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+}
