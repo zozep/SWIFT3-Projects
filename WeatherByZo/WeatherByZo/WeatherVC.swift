4b9b066e649b9834057017659a952bca6411ca67
joseph park <jpark041@gmail.com>
2016-11-29 23:17:06 -0800

    Prototype Cell with correct UI, IBOutlets connected

--- WeatherByZo/WeatherByZo/WeatherVC.swift
+++ WeatherByZo/WeatherByZo/WeatherVC.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class WeatherVC: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
