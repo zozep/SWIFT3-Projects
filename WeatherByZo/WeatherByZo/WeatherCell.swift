bc5752fb03b4684147d8a9723bf44bb8933cedae
joseph park <jpark041@gmail.com>
2016-12-01 23:59:42 -0800

    outlets paired, configuration func written

---
+++ WeatherByZo/WeatherByZo/WeatherCell.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherCell.swift
+//  WeatherByZo
+//
+//  Created by Joseph Park on 12/1/16.
+//  Copyright © 2016 Joseph Park. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var weatherType: UILabel!
+    @IBOutlet weak var highTemp: UILabel!
+    @IBOutlet weak var lowTemp: UILabel!
+    
+    func configureCell(forecast: Forecast) {
+        lowTemp.text = "\(forecast.lowTemp)"
+        highTemp.text = "\(forecast.highTemp)"
+        weatherType.text = forecast.weatherType
+        weatherIcon.image = UIImage(named: forecast.weatherType)
+        dayLabel.text = forecast.date
+    }
+
+
+
+}
