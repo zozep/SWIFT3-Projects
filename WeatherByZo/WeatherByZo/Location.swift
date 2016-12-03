177b36c341829dae0804758acc5cdb203512f732
joseph park <jpark041@gmail.com>
2016-12-02 18:56:40 -0800

    ready to save location data to singleton class for global access

---
+++ WeatherByZo/WeatherByZo/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  WeatherByZo
+//
+//  Created by Joseph Park on 12/2/16.
+//  Copyright © 2016 Joseph Park. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class Location {
+    static var sharedInstance = Location()
+    private init() {}
+    
+    var latitude: Double!
+    var longitude: Double!
+    
+}
