7157733ffcba48ea588b30f3b0521c9336c6f441
joseph park <jpark041@gmail.com>
2016-12-29 10:49:36 -0800

    custom taco cell made with xib

---
+++ POPExp/POPExp/TacoCell.swift
@@ -0,0 +1,29 @@
+//
+//  TacoCell.swift
+//  POPExp
+//
+//  Created by Joseph Park on 12/29/16.
+//  Copyright © 2016 Joseph Park. All rights reserved.
+//
+
+import UIKit
+
+class TacoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var tacoImage: UIImageView!
+    @IBOutlet weak var tacoLabel: UILabel!
+    
+    var taco: Taco!
+    
+    func configureCell(taco: Taco) {
+        self.taco = taco
+        tacoImage.image = UIImage(named: taco.proteinID.rawValue)
+        tacoLabel.text = taco.productName
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
