870467fdb872c259a48a074b8cad9e1ede6fdf48
joseph park <jpark041@gmail.com>
2016-12-29 18:04:57 -0800

    content extension WIP

---
+++ Notifications/MyContentExtension/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  MyContentExtension
+//
+//  Created by Joseph Park on 12/29/16.
+//  Copyright © 2016 Joseph Park. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
