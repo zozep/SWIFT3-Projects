ff0f5d29a7df5d74b3f3e3c326fe7e83b1b544ae
joseph park <jpark041@gmail.com>
2017-01-01 12:43:03 -0800

    fix map, calloutAccessoryControlTapped function made with placemarks and options

--- PokeSearcher/PokeSearcher/ViewController.swift
+++ PokeSearcher/PokeSearcher/ViewController.swift
@@ -128,12 +128,32 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             btn.setImage(UIImage(named: "map"), for: .normal)
             annotationView.rightCalloutAccessoryView = btn
             
-            
         }
         
         //result shows on the map
         return annotationView
     }
-
+    
+    //swiping and showing things on map
+    func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        let loc = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        showSightingsOnMap(location: loc)
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        //tap the pokemon, and popup appears on the map on it, tap the map, then it gives pokemon's position
+        if let anno = view.annotation as? PokeAnnotation {
+            let place = MKPlacemark(coordinate: anno.coordinate)
+            let destination = MKMapItem(placemark: place)
+            
+            destination.name = "Pokemon Sighting"
+            let regionDistance: CLLocationDistance = 1000
+            let regionSpan = MKCoordinateRegionMakeWithDistance(anno.coordinate, regionDistance, regionDistance)
+            
+            let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span), MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving
+                        ] as [String : Any]
+            MKMapItem.openMaps(with: [destination], launchOptions: options)
+        }
+    }
 }
 
