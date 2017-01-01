5e7b95fa372ab090e7ef2df80cf845092bf9afa1
joseph park <jpark041@gmail.com>
2016-12-31 21:38:12 -0800

    initial commit

---
+++ PokeSearcher/PokeSearcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeSearcher
+//
+//  Created by Joseph Park on 12/31/16.
+//  Copyright © 2016 Joseph Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
