b4b35ecee410753b1ef26b4700b87af585015bed
joseph park <jpark041@gmail.com>
2017-02-15 17:08:29 +0700

    button experiment & assets imported

--- pitchChanger/pitchChanger/ViewController.swift
+++ pitchChanger/pitchChanger/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button is pressed")
+        recordingLabel.text = "Recording in Progress"
+    }
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop recording")
+    }
 
 }
 
