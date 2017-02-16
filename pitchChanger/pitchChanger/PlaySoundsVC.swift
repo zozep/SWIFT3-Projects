239556f351bfe2d9babd9e47109f608e9054c268
joseph park <jpark041@gmail.com>
2017-02-16 13:05:00 +0700

    playSound Button function added

--- pitchChanger/pitchChanger/PlaySoundsVC.swift
+++ pitchChanger/pitchChanger/PlaySoundsVC.swift
@@ -36,7 +36,22 @@ class PlaySoundsVC: UIViewController {
     }
     
     @IBAction func playSoundForButton(_ sender: UIButton) {
-        print("Play Sound Button Pressed")
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(.playing)
     }
     
     @IBAction func stopButtonPressed(_ sender: AnyObject) {
