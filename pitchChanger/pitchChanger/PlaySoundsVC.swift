e1be6b1953177048ecd8d9ca2c2a546b02d38e33
joseph park <jpark041@gmail.com>
2017-02-17 22:07:03 +0700

    comment removal

--- pitchChanger/pitchChanger/PlaySoundsVC.swift
+++ pitchChanger/pitchChanger/PlaySoundsVC.swift
@@ -61,18 +61,10 @@ class PlaySoundsVC: UIViewController {
     enum ButtonType: Int {
         case slow = 0, fast, chipmunk, vader, echo, reverb
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
+// MARK: PlaySoundsVC Extension
 extension PlaySoundsVC: AVAudioPlayerDelegate {
     
     // MARK: Alerts
